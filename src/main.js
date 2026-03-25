cabfbad65016eaa1a2b3c3dfe9a6586b74b95aea
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2026-03-25 10:41:35 +0200

    implement header

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const refs = {
+  openMenuBtn: document.querySelector('[data-menu-open]'),
+  closeMenuBtn: document.querySelector('[data-menu-close]'),
+  backdrop: document.querySelector('[data-backdrop]'),
+  menuLinks: document.querySelectorAll('.mobile-nav__link'),
+  body: document.body,
+};
+
+function toggleMenu() {
+  refs.backdrop.classList.toggle('is-open');
+  refs.body.classList.toggle('no-scroll');
+}
+
+refs.openMenuBtn.addEventListener('click', toggleMenu);
+refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+refs.backdrop.addEventListener('click', e => {
+  if (e.target === refs.backdrop) {
+    toggleMenu();
+  }
+
+  if (e.target.classList.contains('mobile-nav__link')) {
+    toggleMenu();
+  }
+});
